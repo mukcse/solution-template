ea60e84482a16912b1a7acf05da854ba3bbc74e1
mukcse <mukcse@gmail.com>
2018-02-06 11:52:20 +0600

    2nd commit

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -2,7 +2,7 @@ package com.tigerit.exam;
 
 
 import static com.tigerit.exam.IO.*;
-import  java.util.*                                                                                                                    ;  
+import  java.util.*;                                                                                                                    ;  
 /**
  * All of your application logic should be placed inside this class.
  * Remember we will load your application from our custom container.
@@ -15,28 +15,112 @@ public class Solution implements Runnable {
         // your application entry point
 
     	Integer T, nT, nD, nC, d, nQ;
-    	String TableName, ColumnName;
-    	int[][][] DbMat = new int[][][];
     	
 
     	T = readLineAsInteger();
     	for(int tc = 1; tc <= T; tc++)
     	{
-    		nT = readLineAsInteger();
-    		for(int it = 1; it <= nT; it++)
+            nT = readLineAsInteger();
+
+            String TableName, ColumnName;
+            Integer[][][] DbMat = new Integer[nT][][];
+            String[][] ColumnNameList = new String[nT][];
+            HashMap<String, Integer> Table = new HashMap<>();
+            HashMap<String, Integer> Column = new HashMap<>();
+
+    		for(int it = 0; it < nT; it++)
     		{
+                Integer ColumnId=0;
     			TableName = readLine();
-    			nC = readLineAsInteger();
-    			nD = readLineAsInteger();
-    			ColumnNameLine = readLine();
-    			String[] ColumnNameList = ColumnNameLine.split(" ");
-    			for(int i=1; i<=nC; i++)
+                Table.put(TableName, it);
+
+
+                String tmp = readLine();
+                String[] tmpA = tmp.split(" ");
+                nC = Integer.parseInt(tmpA[0]);
+                nD = Integer.parseInt(tmpA[1]);
+
+    			String ColumnNameLine = readLine();
+    			ColumnNameList[it] = ColumnNameLine.split(" ");
+                for(String str:ColumnNameList[it]){
+                    Column.put(str, ColumnId++);
+                }
+
+                DbMat[it] = new Integer[nD][nC];
+    			for(int i=0; i<nD; i++)
     			{
-    				for(int j=1; j<=nD; i++)
+                    String tmp2 = readLine();
+                    String[] tmpA2 = tmp2.split(" ");
+                    System.out.print("---->>>>  "+tmpA2.length);
+    				for(int j=0; j<nC; i++)
     				{
+                        DbMat[it][i][j] = Integer.parseInt(tmpA2[j]);
     				}
     			}
     		}
+            nQ = readLineAsInteger();
+            for(int q=0; q<nQ; q++)
+            {
+                HashMap<String, String> table_alias = new HashMap<>();
+                Integer t1,t2,c1,c2;
+
+                //selection
+                String s = readLine();
+                String[] ss = s.split(" |\\,|\\.");
+                Integer sl = ss.length;
+
+                //1st table
+                String as = readLine();
+                String[] aa = as.split(" ");
+                Integer al = aa.length;
+                if(al==2) t1 = Table.get(aa[1]);
+                else
+                {
+                    table_alias.put(aa[2], aa[1]);
+                    t1 = Table.get(table_alias.get(aa[2]));
+                }
+
+
+                //2nd table
+                String bs = readLine();
+                String[] bb = bs.split(" ");
+                Integer bl = bb.length;
+                if(bl==2) t2 = Table.get(bb[1]);
+                else
+                {
+                    table_alias.put(bb[2], bb[1]);
+                    t2 = Table.get(table_alias.get(bb[2]));
+                }
+
+                //condition
+                String c = readLine();
+                String[] cc = c.split(" |\\.");
+                c1=Column.get(cc[2]);
+                c2=Column.get(cc[5]);
+
+                if(ss[1]=="*")
+                {
+                    for(int i=0; i<ColumnNameList[t1].length; i++) System.out.print(ColumnNameList[t1][i]+" ");
+                    for(int i=0; i<ColumnNameList[t2].length; i++) System.out.print(ColumnNameList[t2][i]+" ");
+                    System.out.println();
+                }
+                for(int i=0; i<DbMat[t1].length; i++)
+                {
+                    for(int j=0; j<DbMat[t2].length; j++)
+                    {                     
+                        if(DbMat[t1][i][c1]==DbMat[t2][j][c2])
+                        {
+                            if(ss[1]=="*")
+                            {
+                                for(int i2=0; i2<DbMat[t1].length; i2++) System.out.print(DbMat[t1][i2][c1]+" ");
+                                for(int j2=0; j2<DbMat[t2].length; j2++) System.out.print(DbMat[t2][j2][c2]+" ");
+                                System.out.println();
+                            }   
+                        }
+                    }
+                }
+                System.out.println();
+            }
     	}
     }
 }
